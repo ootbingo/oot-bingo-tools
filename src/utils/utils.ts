cb17e152985b784eab82dfa31cfa237a5df5b4e2
xwmtp <xwillmtp@gmail.com>
2024-01-14 17:12:44 +0100

    Remove @internal

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,5 @@
 /**
  * Calculates the sum of an array of numbers
- * @internal
  * @param numbers
  */
 export function sum(numbers: number[]) {
@@ -9,7 +8,6 @@ export function sum(numbers: number[]) {
 
 /**
  * Calculates the average of an array of numbers
- * @internal
  * @param numbers
  */
 export function average(numbers: number[]) {
